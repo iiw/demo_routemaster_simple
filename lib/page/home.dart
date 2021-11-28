14880e05fc67ebac944b46c964568a7d248b863d
Бузанов Виктор <victor@summer.ac>
2021-11-29 02:01:30 +0300

    back navigation to tab bar index 0

--- lib/page/home.dart
+++ lib/page/home.dart
@@ -7,21 +7,32 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     final tabState = CupertinoTabPage.of(context);
 
-    return CupertinoTabScaffold(
-      controller: tabState.controller,
-      tabBuilder: tabState.tabBuilder,
-      tabBar: CupertinoTabBar(
-        items: const [
-          BottomNavigationBarItem(
-            label: 'Feed',
-            icon: Icon(CupertinoIcons.list_bullet),
+    return WillPopScope(
+        // with nested navigation using tabs we can catch back button click
+        // and navigate to first tab before closing app
+        onWillPop: () async {
+          var willPop = tabState.controller.index == 0;
+          if (!willPop) {
+            // navigate to first tab using its path
+            Routemaster.of(context).replace("/feed");
+          }
+          return willPop;
+        },
+        child: CupertinoTabScaffold(
+          controller: tabState.controller,
+          tabBuilder: tabState.tabBuilder,
+          tabBar: CupertinoTabBar(
+            items: const [
+              BottomNavigationBarItem(
+                label: 'Feed',
+                icon: Icon(CupertinoIcons.list_bullet),
+              ),
+              BottomNavigationBarItem(
+                label: 'Settings',
+                icon: Icon(CupertinoIcons.search),
+              ),
+            ],
           ),
-          BottomNavigationBarItem(
-            label: 'Settings',
-            icon: Icon(CupertinoIcons.search),
-          ),
-        ],
-      ),
-    );
+        ));
   }
 }
